0873d7cfc52e649ff2d40fd47d48cb9202af06a7
hotsuitor <hotsuitor@qq.com>
2023-03-15 13:32:05 +0800

    chore: change baseUrl(#master)

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog2/",
+  base: "/blog/",
 
   locales: {
     // "/": {
